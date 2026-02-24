77c09f9664d271f7e0118a47f76444efed160331
Tai <huynhtai711@gmail.com>
2026-02-24 16:53:00 +0700

    fix: middleware cookie typing

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,13 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookies) {
+        setAll(
+          cookies: {
+            name: string;
+            value: string;
+            options: any;
+          }[]
+        ) {
           cookies.forEach(({ name, value, options }) => {
             response.cookies.set(name, value, options);
           });
